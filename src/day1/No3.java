39a398d78f0ccaac8ff6a0e432dbb8c96b1ca023
Jeongha Im <yimjh3205@gmail.com>
2025-10-14 11:02:47 +0900

    몫 구하기

---
+++ src/day1/No3.java
@@ -0,0 +1,18 @@
+// 몫 구하기
+
+// 문제 설명
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 0 < num1 ≤ 100
+// 0 < num2 ≤ 100
+
+package day1;
+
+public class No3 {
+    public int No3(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
+
