c68c989468c667b441993117ed22324bba9852a8
Jeongha Im <yimjh3205@gmail.com>
2025-10-16 09:39:05 +0900

    짝수의 합

---
+++ src/day3/No9.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class No9 {
+    public int No9(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            } continue;
+        }
+        return sum;
+    }
+}
