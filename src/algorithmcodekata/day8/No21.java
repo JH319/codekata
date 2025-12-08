bb0ff1ad008d0e5c3bc5c0adb492c0bff254d819
Jeongha Im <yimjh3205@gmail.com>
2025-12-08 22:38:14 +0900

    21. 하샤드 수

---
+++ src/algorithmcodekata/day8/No21.java
@@ -0,0 +1,17 @@
+package algorithmcodekata.day8;
+
+public class No21 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if (num % sum == 0) return true;
+        else return false;
+    }
+}
